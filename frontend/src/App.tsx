2ee45416deebed6aead95c859a4459658a5057ee
Ahmad Darmawan <adarwawan@gmail.com>
2026-06-28 13:15:39 +0700

    feat(ui): remove Odds tab from nav

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AppShell } from './components/layout/AppShell';
-import { PlayersPage }     from './pages/PlayersPage';
-import { TeamsPage }       from './pages/TeamsPage';
-import { ScatterPage }     from './pages/ScatterPage';
-import { FixtureOddsPage } from './pages/FixtureOddsPage';
+import { PlayersPage } from './pages/PlayersPage';
+import { TeamsPage }   from './pages/TeamsPage';
+import { ScatterPage } from './pages/ScatterPage';
 
 const queryClient = new QueryClient();
 
@@ -18,7 +17,6 @@ export default function App() {
           <Route path="/:game/players" element={<AppShell><PlayersPage /></AppShell>} />
           <Route path="/:game/teams"   element={<AppShell><TeamsPage /></AppShell>} />
           <Route path="/:game/scatter" element={<AppShell><ScatterPage /></AppShell>} />
-          <Route path="/:game/odds"   element={<AppShell><FixtureOddsPage /></AppShell>} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
